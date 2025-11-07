f4cfcdbfc71346cd3a9513c359040af83cdad3ec
ArthurToso <arthurtoso2003@gmail.com>
2025-11-07 19:29:43 -0300

    adiciona splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.cardview)
     testImplementation(libs.junit)
+    implementation("androidx.core:core-splashscreen:1.2.0")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }

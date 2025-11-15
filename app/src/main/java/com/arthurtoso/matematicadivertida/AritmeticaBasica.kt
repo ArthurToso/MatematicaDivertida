aed4238619c77df1f43abd429480bb73ee7f9e1d
Gabriel-Kaue <gabrielkauecaitan099@gmail.com>
2025-11-15 19:46:25 -0300

    Style: tela da aritmética básica.

--- app/src/main/java/com/arthurtoso/matematicadivertida/AritmeticaBasica.kt
+++ app/src/main/java/com/arthurtoso/matematicadivertida/AritmeticaBasica.kt
@@ -5,7 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+var questaoAtual = 0
+var acertos = 0
 class AritmeticaBasica : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

0411eb68751f04fd0fd4ecb4ef1245449396fc11
Gabriel-Kaue <gabrielkauecaitan099@gmail.com>
2025-11-17 19:32:41 -0300

    feat: gera os números

--- app/src/main/java/com/arthurtoso/matematicadivertida/AritmeticaBasica.kt
+++ app/src/main/java/com/arthurtoso/matematicadivertida/AritmeticaBasica.kt
@@ -5,10 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
 var questaoAtual = 0
 var acertos = 0
 class AritmeticaBasica : AppCompatActivity() {
+    private var operando1: Int = 0
+    private var operando2: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_aritmetica_basica)
@@ -17,5 +23,12 @@ class AritmeticaBasica : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        gerarNumeros()
+        exibirNumeros()
     }
+    private fun gerarNumeros(){
+        operando1 = Random.nextInt(0,9)
+        operando2 = Random.nextInt(0,9)
+    }
+
 }
